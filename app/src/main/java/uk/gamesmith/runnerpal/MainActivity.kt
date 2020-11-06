599087f8b66f471081918f54a0586a29c3c0b5c4
André Wagner <WagnerAndre@JohnDeere.com>
2020-11-06 08:37:31 -0300

    Initial app commit.

---
+++ app/src/main/java/uk/gamesmith/runnerpal/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.gamesmith.runnerpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

60477a70ef624dcbaa442e21e58b2f7ef8a7b1f3
André Wagner <WagnerAndre@JohnDeere.com>
2020-11-07 15:02:29 -0300

    wip

--- app/src/main/java/uk/gamesmith/runnerpal/MainActivity.kt
+++ app/src/main/java/uk/gamesmith/runnerpal/MainActivity.kt
@@ -2,6 +2,7 @@ package uk.gamesmith.runnerpal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
